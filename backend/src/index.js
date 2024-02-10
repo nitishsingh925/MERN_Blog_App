f48839311d47f87375bf9346f96a771e1e140d73
Nitish Singh <nitishsingh925@gmail.com>
2024-02-10 13:18:26 +0530

    connect db bug fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import { app } from "./app.js";
+import connectDB from "./db/index.db.js";
 
 const startServer = async () => {
   try {
+    await connectDB();
     app.listen(process.env.PORT || 5000, () => {
       console.log(` ⚙️  Server is running at port : ${process.env.PORT}`);
     });
   } catch (err) {
-    console.log("Server connection failed !!! ", err);
+    console.log("MONGO db connection failed !!!", err);
   }
 };
 
