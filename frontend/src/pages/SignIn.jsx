3d19b6bd38dbfe7e1a8e00302bdef63e6b55e3e6
Nitish Singh <nitishsingh925@gmail.com>
2024-02-11 19:58:49 +0530

    Add sign-in functionality to SignIn component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,5 +1,121 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const Signin = () => {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password) {
+      setErrorMessage("Please fill all the fields");
+      return;
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/signin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+      if (data.success === true) {
+        setLoading(false);
+        navigate("/");
+      }
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+  return (
+    <div className="flex flex-col-reverse md:flex-row items-center pt-11  dark:bg-neutral-700 dark:text-white ">
+      {/* left */}
+      <div className="md:w-1/2 text-center mx-4 md:mx-14">
+        <Link
+          to="/"
+          className="hidden md:inline-block font-semibold  sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">
+            Nitish's
+          </span>
+          Blog
+        </Link>
+        <p className="py-5 sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl">
+          Dive into the Realm of Thoughts with Nitish's Blog – Your Gateway to
+          Inspiration. Discover, Share, and Connect through Captivating Ideas.
+          Join the Conversation of Minds, Where Every Word Holds Power.
+        </p>
+      </div>
+      {/* right */}
+      <div className="md:w-1/2 mb-5">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md mx-auto p-4 shadow-2xl rounded-md dark:border-2 border-white"
+        >
+          <div className="mb-4">
+            <label htmlFor="username">Your username or Email</label>
+            <input
+              type="text"
+              placeholder="Username or Email"
+              id="username"
+              onChange={handleChange}
+              className="w-full p-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 dark:bg-neutral-700"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              autoComplete="off"
+              onChange={handleChange}
+              className="w-full p-2 mt-1 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 dark:bg-neutral-700"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg font-bold"
+          >
+            {loading ? (
+              <div className="flex justify-center">
+                <img
+                  src="icons/spinner.svg"
+                  alt="spiner"
+                  className="w-6 md:w-7"
+                />
+                <span className="px-2">Loading..</span>
+              </div>
+            ) : (
+              "Sign IN"
+            )}
+          </button>
+          {errorMessage && (
+            <div className="text-center my-3 text-red-600 bg-red-200 rounded-lg  sm:text-xs md:text-sm lg:text-base xl:text-lg 2xl:text-xl">
+              {errorMessage}
+            </div>
+          )}
+        </form>
+        <div className="text-center py-8">
+          <Link to={"/sign-up"}>
+            Have an accoun ? <span className="text-blue-500">Sign Up</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
