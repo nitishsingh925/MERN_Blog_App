fcc596dc9143cb31d2742885bb96ab259f31c77c
Nitish Singh <nitishsingh925@gmail.com>
2024-02-22 15:48:47 +0530

    Add getPosts endpoint to post controller and route

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -28,4 +28,49 @@ const createPost = async (req, res) => {
     throw new ApiError(error);
   }
 };
-export { createPost };
+const getPosts = async (req, res) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 12;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+    const totalPosts = await Post.countDocuments();
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json(
+      new ApiResponse(
+        200,
+        {
+          posts,
+          totalPosts,
+          lastMonthPosts,
+        },
+        "Posts fetched"
+      )
+    );
+  } catch (error) {
+    throw new ApiError(500, error);
+  }
+};
+export { createPost, getPosts };
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/user.middleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPosts } from "../controllers/post.controller.js";
 const router = Router();
 router.post("/create", verifyJWT, createPost);
+router.get("/getposts", getPosts);
 export default router;
